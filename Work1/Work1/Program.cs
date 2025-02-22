705452dda209d56ead23b4b459bdad8f01d319a7
JonyPolker <s.efimov1904@gmail.com>
2025-02-22 22:19:55 +0300

    Add files via upload

---
+++ Work1/Work1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1.
+            int m;
+            m = 1;
+            float d;
+            d = 2;
+            string text;
+            text = "text";
+            bool state;
+            state = true;
+
+            // Задание 2.
+            int skr = 850;
+            const int sec = 60;
+            int benz = 20;
+            string sostvkl = "Включен";
+            const double g = 9.81;
+
+            // Задание 3.
+            float n = 1.2f;
+            int j = (int)1f;
+            ushort f = 50000;
+
+
+            // Задание 4.
+            byte p = 20;
+            byte o = 120;
+            int i = 1111651;
+            int r = 1615145;
+        }
+    }
+}
