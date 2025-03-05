cecd8a5992c9a977b24e646b56fdcab16eb9d524
Sergey <s.efimov1904@gmail.com>
2025-03-05 14:09:48 +0300

    Upload Work11

---
+++ Work11/Work11/Task1/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work11.Task1
+{
+    abstract internal class Animal
+    {
+        public string _name;
+        
+        public Animal(string name)
+        {
+            _name = name;
+        }
+        abstract public void Sound();
+    }
+}
---
+++ Work11/Work11/Task1/Duck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work11.Task1
+{
+    internal class Duck : Animal
+    {
+        public Duck(string name) : base(name)
+        {
+            _name = name;
+        }
+        public override void Sound()
+        {
+            Console.WriteLine($"{_name} крякнул!");
+        }
+    }
+}
