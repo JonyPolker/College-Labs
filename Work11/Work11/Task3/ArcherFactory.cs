f3fb55b1ce872ee89fc3d7bc34573cbbda45736c
Sergey <s.efimov1904@gmail.com>
2025-03-07 22:34:53 +0300

    Add files via upload

---
+++ Work11/Work11/Task3/ArcherFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work11.Task3
+{
+    internal class ArcherFactory : AbstractFactory
+    {
+        private int _money = 50;
+        private int _wood = 50;
+
+        public ArcherFactory(ResourseStorage obj) : base(obj)
+        {
+            rs = obj;
+        }
+        public override void Create()
+        {
+            if(rs.TryGetMoney(_money) == true && rs.TryGetWood(_wood) == true)
+            {
+                rs.Money -= _money;
+                rs.Wood -= _wood;
+                Console.WriteLine("Archer create is complite");
+            }
+        }
+    }
+}
