b158090e0c84d27069761c43601a6b1399a8233a
Sergey <s.efimov1904@gmail.com>
2025-03-07 23:25:35 +0300

    Add files via upload

---
+++ Work11/Work11/Task3/AbstractFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work11.Task3
+{
+    abstract internal class AbstractFactory
+    {
+        private protected ResourseStorage rs;
+        public AbstractFactory(ResourseStorage obj)
+        {
+            rs = obj;
+        }
+
+        public abstract void Create();
+    }
+}
