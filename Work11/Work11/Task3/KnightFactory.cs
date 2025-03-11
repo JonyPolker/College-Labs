a89b89f2888ace4ad3a8b62b9385f93f5652a529
Sergey <s.efimov1904@gmail.com>
2025-03-11 12:01:18 +0300

    Upload work11

---
+++ Work11/Work11/Task3/KnightFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work11.Task3
+{
+    internal class KnightFactory : AbstractFactory
+    {
+        private int _money = 150;
+        private int _iron = 100;
+
+        public KnightFactory(ResourseStorage obj) : base(obj)
+        {
+            rs = obj;
+        }
+        public override void Create()
+        {
+            if (rs.TryGetMoney(_money) == true && rs.TryGetWood(_iron) == true)
+            {
+                rs.Money -= _money;
+                rs.Iron -= _iron;
+                Console.WriteLine("Archer create is complite");
+            }
+        }
+    }
+}
